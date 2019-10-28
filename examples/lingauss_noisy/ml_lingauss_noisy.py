9f0f8cfe0104181b57fd7a7ca45e0a162dca5ce2
peter-nickl <peternickl@gmx.de>
2019-10-29 00:35:16 +0100

    initial commit

---
+++ examples/lingauss_noisy/ml_lingauss_noisy.py
@@ -0,0 +1,53 @@
+import numpy as np
+import numpy.random as npr
+
+from matplotlib import pyplot as plt
+from mimo import distributions
+
+in_dim = 1
+out_dim = 1
+
+_A = 1. * npr.randn(out_dim, in_dim)
+
+nb_samples = 4000
+affine = True
+
+dist1 = distributions.Gaussian(mu=npr.randn(in_dim), sigma=5. * np.diag(npr.rand(in_dim)))
+x = dist1.rvs(size=nb_samples)
+x = np.asarray(x)
+
+dist2 = distributions.LinearGaussian(A=_A, sigma=2.5e-1 * np.eye(out_dim), affine=False)
+
+data = dist2.rvs(size=nb_samples, x=x)
+#
+# plt.figure()
+# plt.plot(data[:, 0], data[:, 1], 'kx')
+# plt.title('Data')
+# plt.show()
+
+#
+# if affine:
+#     out_dim = out_dim + 1
+hypparams = dict(mu=np.zeros((in_dim,)),
+                 kappa=0.05,
+                 psi_niw=np.eye(in_dim),
+                 nu_niw=2 * in_dim + 1,
+                 M=np.zeros((out_dim, in_dim)),
+                 V=1. * np.eye(in_dim),
+                 affine= affine,
+                 psi_mniw=np.eye(out_dim),
+                 nu_mniw=2 * out_dim + 1)
+
+
+prior = distributions.NormalInverseWishartMatrixNormalInverseWishart(**hypparams)
+
+model = distributions.BayesianLinearGaussianWithNoisyInputs(prior)
+model = model.max_likelihood(data)
+print("True transf. x"+"\n", dist1.mu, "\n"+"True covariance x"+"\n", dist1.sigma)
+print("ML transf. x"+"\n", model.mu, "\n"+"ML covariance x"+"\n", model.sigma_niw)
+
+
+print("------------------------------------------------------------------------------------------------")
+print("True transf. y"+"\n", dist2.A, "\n"+"True covariance y"+"\n", dist2.sigma)
+print("ML transf. y"+"\n", model.A, "\n"+"ML covariance y"+"\n", model.sigma)
+
--- mimo/distributions/__init__.py
+++ mimo/distributions/__init__.py
@@ -1,9 +1,14 @@
 from .gaussian import *
 from .matrix import *
-from .lingauss import *
+# from .lingauss import *
 from .gamma import *
 from .wishart import *
 from .categorical import *
 from .dirichlet import *
-from .composite import *
-from .bayesian import *
+# from .composite import *
+# from .bayesian import *
+
+from .lingauss_new import *
+from .composite_new import *
+from .bayesian_new import *
+
