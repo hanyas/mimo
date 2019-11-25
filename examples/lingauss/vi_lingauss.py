0b5dc61792bde1673eef7d10d1aba15d341ebb7a
Hany Abdulsamad <hany.abdulsamad@gmail.com>
2019-11-25 11:00:29 +0100

    update examples of lingauss

--- examples/lingauss/vi_lingauss.py
+++ examples/lingauss/vi_lingauss.py
@@ -11,15 +11,15 @@ _A = 1. * npr.randn(out_dim, in_dim)
 nb_samples = 200
 nb_datasets = 200
 
-dist = distributions.LinearGaussian(A=_A, sigma=2.5e-1 * np.eye(out_dim), affine=False)
+dist = distributions.LinearGaussian(A=_A, sigma=25e-2 * np.eye(out_dim), affine=False)
 data = [dist.rvs(size=nb_samples) for _ in range(nb_datasets)]
 print("True transf."+"\n", dist.A, "\n"+"True sigma"+"\n", dist.sigma)
 
 hypparams = dict(M=np.zeros((out_dim, in_dim)),
                  V=1. * np.eye(in_dim),
-                 affine=False,
                  psi=np.eye(out_dim),
-                 nu=2 * out_dim + 1)
+                 nu=2 * out_dim + 1,
+                 affine=False)
 prior = distributions.MatrixNormalInverseWishart(**hypparams)
 
 model = distributions.BayesianLinearGaussian(prior)
