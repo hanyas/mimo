03dda49d205720cda15809ee45f531090b646d24
Hany Abdulsamad <hany.abdulsamad@gmail.com>
2019-11-26 20:53:59 +0100

    update plotting

--- mimo/util/plot.py
+++ mimo/util/plot.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot as plt
 import tikzplotlib
 
 
-def plot_gaussian(mu, lmbda, color='rnd_psi_mniw', label='', alpha=1.0, ax=None, artists=None):
+def plot_gaussian(mu, lmbda, color='r', label='', alpha=1.0, ax=None, artists=None):
     ax = ax if ax else plt.gca()
 
     t = np.hstack([np.arange(0, 2 * np.pi, 0.01), 0])
@@ -31,14 +31,11 @@ def plot_gaussian(mu, lmbda, color='rnd_psi_mniw', label='', alpha=1.0, ax=None,
     return (line, point) if point else (line,)
 
 
-def violin_plot(data, num_columns=None, tikz_path=None, pdf_path=None, x_label=None, y_label=None, title=None, x_categories=None):
-    def adjacent_values(vals, q1, q3):
-        upper_adjacent_value = q3 + (q3 - q1) * 1.5
-        upper_adjacent_value = np.clip(upper_adjacent_value, q3, vals[-1])
-
-        lower_adjacent_value = q1 - (q3 - q1) * 1.5
-        lower_adjacent_value = np.clip(lower_adjacent_value, vals[0], q1)
-        return lower_adjacent_value, upper_adjacent_value
+def plot_violin_box(data, nb_cols=None,
+                    tikz_path=None, pdf_path=None,
+                    x_label=None, y_label=None,
+                    title=None, x_categories=None,
+                    violin=True, box=True, show=False):
 
     def set_axis_style(ax, labels):
         ax.get_xaxis().set_tick_params(direction='out')
@@ -48,58 +45,43 @@ def violin_plot(data, num_columns=None, tikz_path=None, pdf_path=None, x_label=N
         ax.set_xlim(0.25, len(labels) + 0.75)
         ax.set_xlabel(x_label)
 
-    # fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(9, 4), sharey=True)
-    fig, ax2 = plt.subplots(nrows=1, ncols=1, sharey=True)
+    if box:
+        fig, ax1 = plt.subplots(nrows=1, ncols=1, sharey='all')
+        ax1.boxplot(data, showfliers=True, showmeans=True, meanline=True)
 
-    # ax1.set_title(title)
-    # ax1.set_ylabel(y_label)
-    # ax1.violinplot(data, showmeans=False, showmedians=True, showextrema=False)
+        labels = x_categories
+        set_axis_style(ax1, labels)
+        set_axis_style(ax1, labels)
+        ax1.set_ylabel(y_label)
 
-    # fig.set_title(title)
-    # data = np.ndarray.tolist(data)
+        tikzplotlib.save(tikz_path + '_box.tex', encoding=None)
+        plt.savefig(pdf_path + '_box.pdf')
+        if show:
+            plt.show()
 
-    # ax2.boxplot(data , showfliers = True, showmeans = True, meanline = True)#,  whis=[5, 95])
+    if violin:
+        fig, ax2 = plt.subplots(nrows=1, ncols=1, sharey='all')
+        parts = ax2.violinplot(data, showmeans=False, showmedians=False, showextrema=False)
+        for pc in parts['bodies']:
+            pc.set_edgecolor('black')
+            pc.set_alpha(1)
 
-    # UNCOMMENT FOR VIOLIN PLOT
-    parts = ax2.violinplot( data, showmeans=False, showmedians=False, showextrema=False)
-    for pc in parts['bodies']:
-        # pc.set_facecolor('#D43F3A')
-        pc.set_edgecolor('black')
-        pc.set_alpha(1)
+        quartile1, medians, quartile3 = np.percentile(data, [25, 50, 75], axis=0)
+        if nb_cols != 1:
+            inds = np.arange(1, len(medians) + 1)
+        else:
+            inds = np.arange(1, 2)
 
-    quartile1, medians, quartile3 = np.percentile(data, [25, 50, 75], axis=0)  # axis=1
-    if num_columns != 1:
-        whiskers = np.array([adjacent_values(sorted_array, q1, q3)
-                             for sorted_array, q1, q3 in zip(data, quartile1, quartile3)])
-        whiskersMin, whiskersMax = whiskers[0, :], whiskers[1, :]  # switched from whiskers[:,0], whiskers[:, 1]
-        inds = np.arange(1, len(medians) + 1)
-    else:
-        whiskers = np.array([data, quartile1, quartile3])
-        whiskersMin, whiskersMax = whiskers[0], whiskers[1]
-        inds = np.arange(1, 2)
+        ax2.scatter(inds, medians, marker='o', color='white', s=30, zorder=3)
+        ax2.vlines(inds, quartile1, quartile3, color='k', linestyle='-', lw=5)
 
-    ax2.scatter(inds, medians, marker='o', color='white', s=30, zorder=3)
-    ax2.vlines(inds, quartile1, quartile3, color='k', linestyle='-', lw=5)
-    # ax2.vlines(inds, whiskersMin, whiskersMax, color='k', linestyle='-', lw=1)
+        labels = x_categories
+        set_axis_style(ax2, labels)
+        set_axis_style(ax2, labels)
+        ax2.set_ylabel(y_label)
 
-    # set style for the axes
-    labels = x_categories
+        tikzplotlib.save(tikz_path + '_violin.tex', encoding=None)
+        plt.savefig(pdf_path + '_violin.pdf')
 
-    # for ax in [ax1, ax2]:
-    #     set_axis_style(ax, labels)
-    #     set_axis_style(ax, labels)
-
-    # for ax in [ax1, ax2]:
-    set_axis_style(ax2, labels)
-    set_axis_style(ax2, labels)
-    ax2.set_ylabel(y_label)
-
-    # plt.subplots_adjust(bottom=0.15, wspace=0.05)
-    tikzplotlib.get_tikz_code(figure=fig, filepath=None, figurewidth=None, figureheight=None, textsize=10.0,
-                              tex_relative_path_to_data=None, externalize_tables=False, override_externals=False,
-                              strict=False, wrap=True, add_axis_environment=True, extra_axis_parameters=None,
-                              extra_tikzpicture_parameters=None, dpi=None, show_info=False, include_disclaimer=True,
-                              standalone=False, float_format='{:.15g}', table_row_sep='\n')
-    tikzplotlib.save(tikz_path, encoding=None)
-    plt.savefig(pdf_path)
-    plt.show()
+        if show:
+            plt.show()
