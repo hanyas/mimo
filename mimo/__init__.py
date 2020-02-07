d2899e14df93326525a711de9e4e00f50d6e9afc
Hany Abdulsamad <abdulsamad@ias.informatik.tu-darmstadt.de>
2020-02-07 14:29:35 +0100

    add new envs

---
+++ mimo/__init__.py
@@ -0,0 +1,20 @@
+from gym.envs.registration import register
+
+
+register(
+    id='BouncingBall-DPGLM-v0',
+    entry_point='mimo.envs:BouncingBall',
+    max_episode_steps=1000,
+)
+
+register(
+    id='Pendulum-DPGLM-v0',
+    entry_point='mimo.envs:Pendulum',
+    max_episode_steps=1000,
+)
+
+register(
+    id='Pendulum-DPGLM-v1',
+    entry_point='mimo.envs:PendulumWithCartesianObservation',
+    max_episode_steps=1000,
+)
---
+++ mimo/envs/__init__.py
@@ -0,0 +1,7 @@
+from .pendulum.pendulum import Pendulum
+from .pendulum.pendulum import PendulumWithCartesianObservation
+
+from .cartpole.cartpole import Cartpole
+from .cartpole.cartpole import CartpoleWithCartesianObservation
+
+from .hybrid.bouncing import BouncingBall
