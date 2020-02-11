241370a0b77d1f6ea929e0565e417ec4a3f393cc
Hany Abdulsamad <abdulsamad@ias.informatik.tu-darmstadt.de>
2020-02-11 14:19:49 +0100

    add cartpole env

--- mimo/__init__.py
+++ mimo/__init__.py
@@ -18,3 +18,15 @@ register(
     entry_point='mimo.envs:PendulumWithCartesianObservation',
     max_episode_steps=1000,
 )
+
+register(
+    id='Cartpole-DPGLM-v0',
+    entry_point='mimo.envs:Cartpole',
+    max_episode_steps=1000,
+)
+
+register(
+    id='Cartpole-DPGLM-v1',
+    entry_point='mimo.envs:CartpoleWithCartesianObservation',
+    max_episode_steps=1000,
+)
