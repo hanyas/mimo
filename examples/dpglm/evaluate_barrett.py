17f1b876ae09a5da1df7c575de78aab0fa6c1a70
Hany Abdulsamad <abdulsamad@ias.informatik.tu-darmstadt.de>
2020-02-11 15:32:15 +0100

    update eval scripts

--- examples/dpglm/evaluate_barrett.py
+++ examples/dpglm/evaluate_barrett.py
@@ -104,12 +104,12 @@ def create_job(kwargs):
     dpglm.add_data(data)
 
     for _ in range(args.super_iters):
-        iter = range(args.gibbs_iters) if not args.verbose\
+        gibbs_iter = range(args.gibbs_iters) if not args.verbose\
             else progprint_xrange(args.gibbs_iters)
 
         # Gibbs sampling
         print("Gibbs Sampling")
-        for _ in iter:
+        for _ in gibbs_iter:
             dpglm.resample_model()
 
         if not args.stochastic:
@@ -140,7 +140,6 @@ def parallel_dpglm_inference(nb_jobs=50, **kwargs):
         kwargs['seed'] = n
         kwargs_list.append(kwargs.copy())
 
-    kwargs_list = [kwargs for _ in range(nb_jobs)]
     return Parallel(n_jobs=min(nb_jobs, nb_cores),
                     verbose=10, backend='loky')(map(delayed(create_job), kwargs_list))
 
@@ -153,15 +152,15 @@ if __name__ == "__main__":
     parser.add_argument('--nb_seeds', help='number of seeds', default=1, type=int)
     parser.add_argument('--prior', help='prior type', default='stick-breaking')
     parser.add_argument('--alpha', help='concentration parameter', default=100, type=float)
-    parser.add_argument('--nb_models', help='max number of models', default=25, type=int)
+    parser.add_argument('--nb_models', help='max number of models', default=500, type=int)
     parser.add_argument('--affine', help='affine functions', action='store_true', default='True')
     parser.add_argument('--no_affine', help='non-affine functions', dest='affine', action='store_false')
     parser.add_argument('--super_iters', help='interleaving Gibbs/VI iterations', default=1, type=int)
-    parser.add_argument('--gibbs_iters', help='Gibbs iterations', default=100, type=int)
+    parser.add_argument('--gibbs_iters', help='Gibbs iterations', default=500, type=int)
     parser.add_argument('--stochastic', help='use stochastic VI', action='store_true', default='True')
     parser.add_argument('--deterministic', help='use deterministic VI', dest='stochastic', action='store_false')
     parser.add_argument('--meanfield_iters', help='max VI iterations', default=500, type=int)
-    parser.add_argument('--svi_iters', help='stochastic VI iterations', default=2500, type=int)
+    parser.add_argument('--svi_iters', help='stochastic VI iterations', default=5000, type=int)
     parser.add_argument('--svi_stepsize', help='svi step size', default=5e-4, type=float)
     parser.add_argument('--svi_batchsize', help='svi batch size', default=1024, type=int)
     parser.add_argument('--prediction', help='prediction w/ mode or average', default='average')
@@ -170,83 +169,69 @@ if __name__ == "__main__":
     parser.add_argument('--no_kmeans', help='do not use KMEANS', dest='kmeans', action='store_false')
     parser.add_argument('--verbose', help='show learning progress', action='store_true', default='True')
     parser.add_argument('--mute', help='show no output', dest='verbose', action='store_false')
+    parser.add_argument('--nb_train', help='size of dataset', default=10000, type=int)
 
     args = parser.parse_args()
 
-    import gym
-
-    from mimo.util.data import sample_env
-
     np.random.seed(1337)
 
-    env = gym.make('Pendulum-DPGLM-v1')
-    env._max_episode_steps = 5000
-    env.unwrapped._dt = 0.01
-    env.unwrapped._sigma = 1e-16
-    env.seed(1337)
+    import scipy as sc
+    from scipy import io
 
-    dm_obs = env.observation_space.shape[0]
-    dm_act = env.action_space.shape[0]
+    # training data
+    data = sc.io.loadmat(args.datapath + '/Barrett/ias_real_barrett_data.mat')
+    train_data = np.hstack((data['X_train'], data['Y_train']))
 
-    nb_train_rollouts, nb_train_steps = 25, 250
-    nb_test_rollouts, nb_test_steps = 10, 250
+    nb_train = args.nb_train
+    train_choice = np.random.choice(len(train_data), nb_train)
+    train_input, train_target = train_data[train_choice, :21], train_data[train_choice, 21:]
 
-    train_obs, train_act = sample_env(env, nb_train_rollouts, nb_train_steps)
-    test_obs, test_act = sample_env(env, nb_test_rollouts, nb_test_steps)
+    # test data
+    test_data = np.hstack((data['X_test'], data['Y_test']))
 
-    train_input = np.vstack([np.hstack((_x[:-1, :], _u[:-1, :])) for _x, _u in zip(train_obs, train_act)])
-    train_target = np.vstack([_x[1:, :] - _x[:-1, :] for _x in train_obs])
+    test_choice = np.random.choice(len(test_data), len(test_data))
+    test_input, test_target = test_data[test_choice, 21:], test_data[test_choice, 21:]
 
-    # train_data = {'input': train_input, 'target': train_target}
-    #
-    # dpglms = parallel_dpglm_inference(nb_jobs=args.nb_seeds,
-    #                                   train_data=train_data,
-    #                                   arguments=args)
-    #
-    # from mimo.util.prediction import meanfield_forcast
-    #
-    # prediction = meanfield_forcast(dpglms[0], test_obs[0][0, :],
-    #                                horizon=len(test_act[0]),
-    #                                exogenous=test_act[0], incremental=True)
-    #
-    # plt.figure()
-    # plt.plot(test_obs[0])
-    # plt.plot(prediction)
-
+    # scale training data
     from sklearn.decomposition import PCA
-    input_scaler = PCA(n_components=dm_obs + dm_act, whiten=True)
-    target_scaler = PCA(n_components=dm_obs, whiten=True)
+    input_scaler = PCA(n_components=21, whiten=True)
+    target_scaler = PCA(n_components=7, whiten=True)
 
     input_scaler.fit(train_input)
     target_scaler.fit(train_target)
 
-    train_data = {'input': input_scaler.transform(train_input),
-                  'target': target_scaler.transform(train_target)}
+    scaled_train_data = {'input': input_scaler.transform(train_input),
+                         'target': target_scaler.transform(train_target)}
 
+    # train
     dpglms = parallel_dpglm_inference(nb_jobs=args.nb_seeds,
-                                      train_data=train_data,
+                                      train_data=scaled_train_data,
                                       arguments=args)
 
-    from mimo.util.prediction import meanfield_forcast
+    from mimo.util.prediction import parallel_meanfield_prediction
+    from sklearn.metrics import explained_variance_score, mean_squared_error
 
-    idx = np.random.choice(len(test_obs))
-    prediction = meanfield_forcast(dpglms[0], test_obs[idx][0, :],
-                                   exogenous=test_act[idx],
-                                   horizon=len(test_act[idx]),
-                                   incremental=True,
-                                   input_scaler=input_scaler,
-                                   target_scaler=target_scaler)
+    # predict on train data
+    train_predict, _, _ = parallel_meanfield_prediction(dpglms[0], train_input,
+                                                        prediction=args.prediction,
+                                                        input_scaler=input_scaler,
+                                                        target_scaler=target_scaler)
 
-    plt.figure()
-    plt.plot(test_obs[idx])
-    plt.plot(prediction)
+    train_evar = explained_variance_score(train_predict, train_target)
+    train_mse = mean_squared_error(train_predict, train_target)
+    train_smse = train_mse / np.var(train_target, axis=0)
 
-    plt.show()
+    print('TRAIN - EVAR:', train_evar, 'MSE:', train_mse, 'SMSE:', train_smse, 'Compnents:', len(dpglms[0].used_labels))
 
-    # from mimo.util.prediction import kstep_error
-    # print(kstep_error(dpglms[0],
-    #                   test_obs, test_act,
-    #                   horizon=1,
-    #                   input_scaler=input_scaler,
-    #                   target_scaler=target_scaler))
-    #
+    # predict on test data
+    test_predict, _, _ = parallel_meanfield_prediction(dpglms[0], test_input,
+                                                       prediction=args.prediction,
+                                                       input_scaler=input_scaler,
+                                                       target_scaler=target_scaler)
+
+    test_evar = explained_variance_score(test_predict, test_target)
+    test_mse = mean_squared_error(test_predict, test_target)
+    test_smse = test_mse / np.var(test_target, axis=0)
+
+    print('TEST - EVAR:', test_evar, 'MSE:', test_mse, 'SMSE:', test_smse, 'Compnents:', len(dpglms[0].used_labels))
+
