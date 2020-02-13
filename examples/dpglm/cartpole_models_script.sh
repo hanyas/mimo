ba73258dcfe8f455dd2495b44428b9d488aa3761
Hany Abdulsamad <abdulsamad@ias.informatik.tu-darmstadt.de>
2020-02-13 11:46:47 +0100

    add evaluation bash scripts

---
+++ examples/dpglm/cartpole_models_script.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+python evaluate_cartpole.py --name models --gibbs_iters 100 --nb_seeds 25 --prior stick-breaking --alpha 10.0 --nb_models 50 --mute &
+python evaluate_cartpole.py --name models --gibbs_iters 100 --nb_seeds 25 --prior stick-breaking --alpha 10.0 --nb_models 75 --mute &
+python evaluate_cartpole.py --name models --gibbs_iters 100 --nb_seeds 25 --prior stick-breaking --alpha 10.0 --nb_models 100 --mute &
+python evaluate_cartpole.py --name models --gibbs_iters 100 --nb_seeds 25 --prior stick-breaking --alpha 10.0 --nb_models 125 --mute &
+python evaluate_cartpole.py --name models --gibbs_iters 100 --nb_seeds 25 --prior stick-breaking --alpha 10.0 --nb_models 150 --mute &
+
+python evaluate_cartpole.py --name models --gibbs_iters 100 --nb_seeds 25 --prior dirichlet --alpha 1.0 --nb_models 50 --mute &
+python evaluate_cartpole.py --name models --gibbs_iters 100 --nb_seeds 25 --prior dirichlet --alpha 1.0 --nb_models 75 --mute &
+python evaluate_cartpole.py --name models --gibbs_iters 100 --nb_seeds 25 --prior dirichlet --alpha 1.0 --nb_models 100 --mute &
+python evaluate_cartpole.py --name models --gibbs_iters 100 --nb_seeds 25 --prior dirichlet --alpha 1.0 --nb_models 125 --mute &
+python evaluate_cartpole.py --name models --gibbs_iters 100 --nb_seeds 25 --prior dirichlet --alpha 1.0 --nb_models 150 --mute
