3d7bfc96a93d310b4e1983a1fc1bf4fcaf2fa7d4
Hany Abdulsamad <abdulsamad@ias.informatik.tu-darmstadt.de>
2020-02-15 22:36:29 +0100

    update setup

--- setup.py
+++ setup.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# @Filename: setup.py
-# @Date: 2019-06-16-21-53
-# @Author: Hany Abdulsamad
-# @Contact: hany@robot-learning.de
-
 from setuptools import setup
 
 setup(name='mimo',
