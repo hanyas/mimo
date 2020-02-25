41d312313ba5bc56a459707f27b87e1699e115b7
Hany Abdulsamad <abdulsamad@ias.informatik.tu-darmstadt.de>
2020-02-25 16:29:24 +0100

    update control eval scripts

--- examples/dpglm/uai2020/control/pendulum_horizon_script.sh
+++ examples/dpglm/uai2020/control/pendulum_horizon_script.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-python evaluate_pendulum.py --name horizon --gibbs_iters 1 --nb_seeds 10 --prior stick-breaking --alpha 10.0 --nb_models 30 --horizon 1 --mute &
-python evaluate_pendulum.py --name horizon --gibbs_iters 1 --nb_seeds 10 --prior stick-breaking --alpha 10.0 --nb_models 30 --horizon 5 --mute &
-python evaluate_pendulum.py --name horizon --gibbs_iters 1 --nb_seeds 10 --prior stick-breaking --alpha 10.0 --nb_models 30 --horizon 10 --mute &
-python evaluate_pendulum.py --name horizon --gibbs_iters 1 --nb_seeds 10 --prior stick-breaking --alpha 10.0 --nb_models 30 --horizon 15 --mute &
-python evaluate_pendulum.py --name horizon --gibbs_iters 1 --nb_seeds 10 --prior stick-breaking --alpha 10.0 --nb_models 30 --horizon 20 --mute &
-python evaluate_pendulum.py --name horizon --gibbs_iters 1 --nb_seeds 10 --prior stick-breaking --alpha 10.0 --nb_models 30 --horizon 25 --mute
+python evaluate_pendulum.py --task horizon --gibbs_iters 1 --nb_seeds 25 --prior stick-breaking --alpha 10.0 --nb_models 30 --horizon 1 --mute &
+python evaluate_pendulum.py --task horizon --gibbs_iters 1 --nb_seeds 25 --prior stick-breaking --alpha 10.0 --nb_models 30 --horizon 5 --mute &
+python evaluate_pendulum.py --task horizon --gibbs_iters 1 --nb_seeds 25 --prior stick-breaking --alpha 10.0 --nb_models 30 --horizon 10 --mute &
+python evaluate_pendulum.py --task horizon --gibbs_iters 1 --nb_seeds 25 --prior stick-breaking --alpha 10.0 --nb_models 30 --horizon 15 --mute &
+python evaluate_pendulum.py --task horizon --gibbs_iters 1 --nb_seeds 25 --prior stick-breaking --alpha 10.0 --nb_models 30 --horizon 20 --mute &
+python evaluate_pendulum.py --task horizon --gibbs_iters 1 --nb_seeds 25 --prior stick-breaking --alpha 10.0 --nb_models 30 --horizon 25 --mute
 
-#python evaluate_pendulum.py --name horizon --gibbs_iters 1 --nb_seeds 10 --prior dirichlet --alpha 1.0 --nb_models 30 --horizon 1 --mute &
-#python evaluate_pendulum.py --name horizon --gibbs_iters 1 --nb_seeds 10 --prior dirichlet --alpha 1.0 --nb_models 30 --horizon 5 --mute &
-#python evaluate_pendulum.py --name horizon --gibbs_iters 1 --nb_seeds 10 --prior dirichlet --alpha 1.0 --nb_models 30 --horizon 10 --mute &
-#python evaluate_pendulum.py --name horizon --gibbs_iters 1 --nb_seeds 10 --prior dirichlet --alpha 1.0 --nb_models 30 --horizon 15 --mute &
-#python evaluate_pendulum.py --name horizon --gibbs_iters 1 --nb_seeds 10 --prior dirichlet --alpha 1.0 --nb_models 30 --horizon 20 --mute &
-#python evaluate_pendulum.py --name horizon --gibbs_iters 1 --nb_seeds 10 --prior dirichlet --alpha 1.0 --nb_models 30 --horizon 25 --mute
+python evaluate_pendulum.py --task horizon --gibbs_iters 1 --nb_seeds 25 --prior dirichlet --alpha 1.0 --nb_models 30 --horizon 1 --mute &
+python evaluate_pendulum.py --task horizon --gibbs_iters 1 --nb_seeds 25 --prior dirichlet --alpha 1.0 --nb_models 30 --horizon 5 --mute &
+python evaluate_pendulum.py --task horizon --gibbs_iters 1 --nb_seeds 25 --prior dirichlet --alpha 1.0 --nb_models 30 --horizon 10 --mute &
+python evaluate_pendulum.py --task horizon --gibbs_iters 1 --nb_seeds 25 --prior dirichlet --alpha 1.0 --nb_models 30 --horizon 15 --mute &
+python evaluate_pendulum.py --task horizon --gibbs_iters 1 --nb_seeds 25 --prior dirichlet --alpha 1.0 --nb_models 30 --horizon 20 --mute &
+python evaluate_pendulum.py --task horizon --gibbs_iters 1 --nb_seeds 25 --prior dirichlet --alpha 1.0 --nb_models 30 --horizon 25 --mute
