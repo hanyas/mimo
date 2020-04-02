205c21549134f00b9ec5a98b04881df6db245452
Hany Abdulsamad <abdulsamad@ias.informatik.tu-darmstadt.de>
2020-04-02 14:41:35 +0200

    update control examples

--- examples/dpglm/uai2020/control/cartpole_horizon_script.sh
+++ examples/dpglm/uai2020/control/cartpole_horizon_script.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-python evaluate_cartpole.py --task horizon --gibbs_iters 1 --nb_seeds 25 --prior stick-breaking --alpha 10.0 --nb_models 100 --horizon 1 --mute &
-python evaluate_cartpole.py --task horizon --gibbs_iters 1 --nb_seeds 25 --prior stick-breaking --alpha 10.0 --nb_models 100 --horizon 5 --mute &
-python evaluate_cartpole.py --task horizon --gibbs_iters 1 --nb_seeds 25 --prior stick-breaking --alpha 10.0 --nb_models 100 --horizon 10 --mute &
-python evaluate_cartpole.py --task horizon --gibbs_iters 1 --nb_seeds 25 --prior stick-breaking --alpha 10.0 --nb_models 100 --horizon 15 --mute &
-python evaluate_cartpole.py --task horizon --gibbs_iters 1 --nb_seeds 25 --prior stick-breaking --alpha 10.0 --nb_models 100 --horizon 20 --mute &
-python evaluate_cartpole.py --task horizon --gibbs_iters 1 --nb_seeds 25 --prior stick-breaking --alpha 10.0 --nb_models 100 --horizon 25 --mute
+#python evaluate_cartpole.py --task horizon --gibbs_iters 100 --stochastic --nb_seeds 25 --prior stick-breaking --alpha 50.0 --nb_models 100 --horizon 1 --mute &
+#python evaluate_cartpole.py --task horizon --gibbs_iters 100 --stochastic --nb_seeds 25 --prior stick-breaking --alpha 50.0 --nb_models 100 --horizon 5 --mute &
+#python evaluate_cartpole.py --task horizon --gibbs_iters 100 --stochastic --nb_seeds 25 --prior stick-breaking --alpha 50.0 --nb_models 100 --horizon 10 --mute &
+#python evaluate_cartpole.py --task horizon --gibbs_iters 100 --stochastic --nb_seeds 25 --prior stick-breaking --alpha 50.0 --nb_models 100 --horizon 15 --mute &
+#python evaluate_cartpole.py --task horizon --gibbs_iters 100 --stochastic --nb_seeds 25 --prior stick-breaking --alpha 50.0 --nb_models 100 --horizon 20 --mute &
+#python evaluate_cartpole.py --task horizon --gibbs_iters 100 --stochastic --nb_seeds 25 --prior stick-breaking --alpha 50.0 --nb_models 100 --horizon 25 --mute
 
-python evaluate_cartpole.py --task horizon --gibbs_iters 1 --nb_seeds 25 --prior dirichlet --alpha 1.0 --nb_models 50 --horizon 1 --mute &
-python evaluate_cartpole.py --task horizon --gibbs_iters 1 --nb_seeds 25 --prior dirichlet --alpha 1.0 --nb_models 50 --horizon 5 --mute &
-python evaluate_cartpole.py --task horizon --gibbs_iters 1 --nb_seeds 25 --prior dirichlet --alpha 1.0 --nb_models 50 --horizon 10 --mute &
-python evaluate_cartpole.py --task horizon --gibbs_iters 1 --nb_seeds 25 --prior dirichlet --alpha 1.0 --nb_models 50 --horizon 15 --mute &
-python evaluate_cartpole.py --task horizon --gibbs_iters 1 --nb_seeds 25 --prior dirichlet --alpha 1.0 --nb_models 50 --horizon 20 --mute &
-python evaluate_cartpole.py --task horizon --gibbs_iters 1 --nb_seeds 25 --prior dirichlet --alpha 1.0 --nb_models 50 --horizon 25 --mute
+python evaluate_cartpole.py --task horizon --gibbs_iters 100 --stochastic --nb_seeds 25 --prior dirichlet --alpha 1.0 --nb_models 100 --horizon 1 --mute &
+python evaluate_cartpole.py --task horizon --gibbs_iters 100 --stochastic --nb_seeds 25 --prior dirichlet --alpha 1.0 --nb_models 100 --horizon 5 --mute &
+python evaluate_cartpole.py --task horizon --gibbs_iters 100 --stochastic --nb_seeds 25 --prior dirichlet --alpha 1.0 --nb_models 100 --horizon 10 --mute &
+python evaluate_cartpole.py --task horizon --gibbs_iters 100 --stochastic --nb_seeds 25 --prior dirichlet --alpha 1.0 --nb_models 100 --horizon 15 --mute &
+python evaluate_cartpole.py --task horizon --gibbs_iters 100 --stochastic --nb_seeds 25 --prior dirichlet --alpha 1.0 --nb_models 100 --horizon 20 --mute &
+python evaluate_cartpole.py --task horizon --gibbs_iters 100 --stochastic --nb_seeds 25 --prior dirichlet --alpha 1.0 --nb_models 100 --horizon 25 --mute
