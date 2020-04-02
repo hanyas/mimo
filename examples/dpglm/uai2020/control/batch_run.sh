c94a507faac4c3a541f7e86e1bb2788c387e4026
Hany Abdulsamad <abdulsamad@ias.informatik.tu-darmstadt.de>
2020-04-02 14:41:35 +0200

    update control examples

--- examples/dpglm/uai2020/control/batch_run.sh
+++ examples/dpglm/uai2020/control/batch_run.sh
@@ -2,8 +2,8 @@
 
 ./pendulum_alpha_script.sh &&
 ./pendulum_models_script.sh &&
-./pendulum_horizon_script.sh &&
+./pendulum_horizon_script.sh 
 
-./cartpole_alpha_script.sh &&
-./cartpole_models_script.sh &&
-./pendulum_horizon_script.sh
+# ./cartpole_alpha_script.sh &&
+# ./cartpole_models_script.sh &&
+# ./pendulum_horizon_script.sh
--- examples/dpglm/uai2020/control/evaluate_cartpole.py
+++ examples/dpglm/uai2020/control/evaluate_cartpole.py
@@ -7,6 +7,7 @@ import numpy.random as npr
 import mimo
 from mimo import distributions, mixture
 from mimo.util.text import progprint_xrange
+from mimo.util.general import near_pd
 
 import argparse
 
@@ -27,8 +28,8 @@ def create_job(kwargs):
     input_dim = input.shape[-1]
     target_dim = target.shape[-1]
 
-    # set random seed
-    np.random.seed(seed)
+    # # set random seed
+    # np.random.seed(seed)
 
     nb_params = input_dim
     if args.affine:
@@ -42,7 +43,8 @@ def create_job(kwargs):
         for n in range(args.nb_models):
             # initialize Normal
             mu_input = km.cluster_centers_[n, :input_dim]
-            psi_niw = 1e0
+            _psi_niw = np.cov(input[km.labels_ == n], bias=False, rowvar=False)
+            psi_niw = np.diag(near_pd(np.atleast_2d(_psi_niw)))
             kappa = 1e-2
 
             # initialize Matrix-Normal
@@ -62,8 +64,6 @@ def create_job(kwargs):
             components_prior.append(aux)
     else:
         # initialize Normal
-        mu_low = np.min(input, axis=0)
-        mu_high = np.max(input, axis=0)
         psi_niw = 1e0
         kappa = 1e-2
 
@@ -72,12 +72,12 @@ def create_job(kwargs):
         V = 1e3 * np.eye(nb_params)
 
         for n in range(args.nb_models):
-            components_hypparams = dict(mu=npr.uniform(mu_low, mu_high, size=input_dim),
+            components_hypparams = dict(mu=np.zeros((input_dim, )),
                                         kappa=kappa, psi_niw=np.eye(input_dim) * psi_niw,
-                                        nu_niw=input_dim + 1,
+                                        nu_niw=input_dim + 1 + 100,
                                         M=np.zeros((target_dim, nb_params)),
                                         affine=args.affine, V=V,
-                                        nu_mniw=target_dim + 1,
+                                        nu_mniw=target_dim + 1 + 100,
                                         psi_mniw=np.eye(target_dim) * psi_mniw)
 
             aux = distributions.NormalInverseWishartMatrixNormalInverseWishart(**components_hypparams)
@@ -94,10 +94,12 @@ def create_job(kwargs):
     # define model
     if args.prior == 'stick-breaking':
         dpglm = mixture.Mixture(gating=distributions.BayesianCategoricalWithStickBreaking(gating_prior),
-                                components=[distributions.BayesianLinearGaussianWithNoisyInputs(components_prior[i]) for i in range(args.nb_models)])
+                                components=[distributions.BayesianLinearGaussianWithNoisyInputs(components_prior[i])
+                                            for i in range(args.nb_models)])
     else:
         dpglm = mixture.Mixture(gating=distributions.BayesianCategoricalWithDirichlet(gating_prior),
-                                components=[distributions.BayesianLinearGaussianWithNoisyInputs(components_prior[i]) for i in range(args.nb_models)])
+                                components=[distributions.BayesianLinearGaussianWithNoisyInputs(components_prior[i])
+                                            for i in range(args.nb_models)])
     dpglm.add_data(data)
 
     for _ in range(args.super_iters):
@@ -139,8 +141,10 @@ def create_job(kwargs):
 def parallel_dpglm_inference(nb_jobs=50, **kwargs):
     kwargs_list = []
     for n in range(nb_jobs):
-        kwargs['seed'] = n
-        kwargs_list.append(kwargs.copy())
+        _kwargs = {'seed': kwargs['arguments'].seed,
+                   'train_data': kwargs['data_dicts'][n],
+                   'arguments': kwargs['arguments']}
+        kwargs_list.append(_kwargs)
 
     return Parallel(n_jobs=min(nb_jobs, nb_cores),
                     verbose=10, backend='loky')(map(delayed(create_job), kwargs_list))
@@ -151,27 +155,27 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Evaluate DPGLM with a Stick-breaking prior')
     parser.add_argument('--datapath', help='path to dataset', default=os.path.abspath(mimo.__file__ + '/../../datasets'))
     parser.add_argument('--evalpath', help='path to evaluation', default=os.path.abspath(mimo.__file__ + '/../../evaluation/uai2020'))
-    parser.add_argument('--nb_seeds', help='number of seeds', default=1, type=int)
+    parser.add_argument('--nb_seeds', help='number of seeds', default=25, type=int)
     parser.add_argument('--prior', help='prior type', default='stick-breaking')
-    parser.add_argument('--alpha', help='concentration parameter', default=10, type=float)
-    parser.add_argument('--nb_models', help='max number of models', default=50, type=int)
+    parser.add_argument('--alpha', help='concentration parameter', default=50, type=float)
+    parser.add_argument('--nb_models', help='max number of models', default=250, type=int)
     parser.add_argument('--affine', help='affine functions', action='store_true', default=True)
     parser.add_argument('--no_affine', help='non-affine functions', dest='affine', action='store_false')
     parser.add_argument('--super_iters', help='interleaving Gibbs/VI iterations', default=1, type=int)
-    parser.add_argument('--gibbs_iters', help='Gibbs iterations', default=100, type=int)
+    parser.add_argument('--gibbs_iters', help='Gibbs iterations', default=1, type=int)
     parser.add_argument('--stochastic', help='use stochastic VI', action='store_true', default=False)
     parser.add_argument('--no_stochastic', help='do not use stochastic VI', dest='stochastic', action='store_false')
     parser.add_argument('--deterministic', help='use deterministic VI', action='store_true', default=True)
     parser.add_argument('--no_deterministic', help='do not use deterministic VI', dest='deterministic', action='store_false')
-    parser.add_argument('--meanfield_iters', help='max VI iterations', default=1000, type=int)
-    parser.add_argument('--svi_iters', help='stochastic VI iterations', default=1000, type=int)
+    parser.add_argument('--meanfield_iters', help='max VI iterations', default=5000, type=int)
+    parser.add_argument('--svi_iters', help='stochastic VI iterations', default=500, type=int)
     parser.add_argument('--svi_stepsize', help='svi step size', default=5e-4, type=float)
     parser.add_argument('--svi_batchsize', help='svi batch size', default=512, type=int)
     parser.add_argument('--prediction', help='prediction w/ mode or average', default='average')
     parser.add_argument('--earlystop', help='stopping criterion for VI', default=1e-2, type=float)
-    parser.add_argument('--kmeans', help='init with KMEANS', action='store_true', default=True)
+    parser.add_argument('--kmeans', help='init with KMEANS', action='store_true', default=False)
     parser.add_argument('--no_kmeans', help='do not use KMEANS', dest='kmeans', action='store_false')
-    parser.add_argument('--verbose', help='show learning progress', action='store_true', default=True)
+    parser.add_argument('--verbose', help='show learning progress', action='store_true', default=False)
     parser.add_argument('--mute', help='show no output', dest='verbose', action='store_false')
     parser.add_argument('--seed', help='choose seed', default=1337, type=int)
     parser.add_argument('--horizon', help='horizon prediction', default=1, type=int)
@@ -181,14 +185,14 @@ if __name__ == "__main__":
 
     import gym
 
-    from mimo.util.data import sample_env
+    from mimo.util.general import sample_env
 
     np.random.seed(args.seed)
 
     env = gym.make('Cartpole-DPGLM-v1')
     env._max_episode_steps = 5000
     env.unwrapped._dt = 0.01
-    env.unwrapped._sigma = 1e-4
+    env.unwrapped._sigma = 1e-2
     env.seed(args.seed)
 
     dm_obs = env.observation_space.shape[0]
@@ -200,21 +204,39 @@ if __name__ == "__main__":
     train_obs, train_act = sample_env(env, nb_train_rollouts, nb_train_steps)
     test_obs, test_act = sample_env(env, nb_test_rollouts, nb_test_steps)
 
-    train_input = np.vstack([np.hstack((_x[:-1, :], _u[:-1, :])) for _x, _u in zip(train_obs, train_act)])
-    train_target = np.vstack([_x[1:, :] - _x[:-1, :] for _x in train_obs])
+    _input = np.vstack([np.hstack((_x[:-1, :], _u[:-1, :])) for _x, _u in zip(train_obs, train_act)])
+    _target = np.vstack([_x[1:, :] - _x[:-1, :] for _x in train_obs])
+
+    train_data = np.hstack((_input, _target))
+
+    # shuffle data
+    from sklearn.utils import shuffle
+    train_data = shuffle(train_data)
 
     from sklearn.decomposition import PCA
     input_scaler = PCA(n_components=dm_obs + dm_act, whiten=True)
     target_scaler = PCA(n_components=dm_obs, whiten=True)
 
-    input_scaler.fit(train_input)
-    target_scaler.fit(train_target)
+    input_scaler.fit(train_data[:, :dm_obs + dm_act])
+    target_scaler.fit(train_data[:, dm_obs + dm_act:])
 
-    train_data = {'input': input_scaler.transform(train_input),
-                  'target': target_scaler.transform(train_target)}
+    # split to nb_seeds train datasets
+    from sklearn.model_selection import ShuffleSplit
+    spliter = ShuffleSplit(n_splits=args.nb_seeds, test_size=0.2)
 
+    train_inputs, train_targets = [], []
+    for train_index, _ in spliter.split(train_data):
+        train_inputs.append(train_data[train_index, :dm_obs + dm_act])
+        train_targets.append(train_data[train_index, dm_obs + dm_act:])
+
+    train_dicts = []
+    for train_input, train_target in zip(train_inputs, train_targets):
+        train_dicts.append({'input': input_scaler.transform(train_input),
+                            'target': target_scaler.transform(train_target)})
+
+    # train
     dpglms = parallel_dpglm_inference(nb_jobs=args.nb_seeds,
-                                      train_data=train_data,
+                                      data_dicts=train_dicts,
                                       arguments=args)
 
     # from mimo.util.prediction import meanfield_forcast
@@ -228,18 +250,15 @@ if __name__ == "__main__":
     #                                target_scaler=target_scaler)
     #
     # plt.figure()
-    # plt.plot(test_obs[idx])
+    # plt.plot(test_obs[idx][1:, :])
     # plt.plot(prediction)
     #
     # plt.show()
 
+    from mimo.util.prediction import kstep_error, parallel_meanfield_prediction
+
     test_input = np.vstack([np.hstack((_x[:-1, :], _u[:-1, :])) for _x, _u in zip(test_obs, test_act)])
     test_target = np.vstack([_x[1:, :] - _x[:-1, :] for _x in test_obs])
-
-    test_data = np.hstack((input_scaler.transform(test_input),
-                           target_scaler.transform(test_target)))
-
-    from mimo.util.prediction import kstep_error
 
     mse, smse, evar, nb_models, duration, nlpd = [], [], [], [], [], []
     for dpglm in dpglms:
@@ -248,7 +267,8 @@ if __name__ == "__main__":
             kstep_error(dpglm, test_obs, test_act, horizon=args.horizon,
                         input_scaler=input_scaler, target_scaler=target_scaler)
 
-        _nlpd = - 1.0 * dpglm.predictive_log_likelihood(test_data)
+        _nlpd = parallel_meanfield_prediction(dpglm, test_input, target=test_target,
+                                              input_scaler=input_scaler, target_scaler=target_scaler)[3]
 
         mse.append(_mse)
         smse.append(_smse)
