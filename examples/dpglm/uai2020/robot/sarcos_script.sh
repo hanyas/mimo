79c37ec287bb980d70c3222bf1ac624b406f0660
Hany Abdulsamad <abdulsamad@ias.informatik.tu-darmstadt.de>
2020-04-02 14:42:06 +0200

    update robot examples

--- examples/dpglm/uai2020/robot/sarcos_script.sh
+++ examples/dpglm/uai2020/robot/sarcos_script.sh
@@ -1,25 +1,9 @@
 #!/bin/sh
 
-python evaluate_sarcos.py --nb_train 40000 --prior stick-breaking --alpha 500 --gibbs_iters 100 --stochastic --seed 1 --nb_models 1000 --mute &
-python evaluate_sarcos.py --nb_train 40000 --prior stick-breaking --alpha 500 --gibbs_iters 100 --stochastic --seed 2 --nb_models 1000 --mute &
-python evaluate_sarcos.py --nb_train 40000 --prior stick-breaking --alpha 500 --gibbs_iters 100 --stochastic --seed 3 --nb_models 1000 --mute &
-python evaluate_sarcos.py --nb_train 40000 --prior stick-breaking --alpha 500 --gibbs_iters 100 --stochastic --seed 4 --nb_models 1000 --mute &
-python evaluate_sarcos.py --nb_train 40000 --prior stick-breaking --alpha 500 --gibbs_iters 100 --stochastic --seed 5 --nb_models 1000 --mute
+#python evaluate_sarcos.py --prior stick-breaking --alpha 500 --gibbs_iters 25 --nb_seeds 5 --nb_models 2500 --mute
+#python evaluate_sarcos.py --prior stick-breaking --alpha 2500 --gibbs_iters 25 --nb_seeds 5 --nb_models 2500 --mute
+#python evaluate_sarcos.py --prior stick-breaking --alpha 5000 --gibbs_iters 25 --nb_seeds 5 --nb_models 2500 --mute
 
-python evaluate_sarcos.py --nb_train 40000 --prior stick-breaking --alpha 1000 --gibbs_iters 100 --stochastic --seed 1 --nb_models 1000 --mute &
-python evaluate_sarcos.py --nb_train 40000 --prior stick-breaking --alpha 1000 --gibbs_iters 100 --stochastic --seed 2 --nb_models 1000 --mute &
-python evaluate_sarcos.py --nb_train 40000 --prior stick-breaking --alpha 1000 --gibbs_iters 100 --stochastic --seed 3 --nb_models 1000 --mute &
-python evaluate_sarcos.py --nb_train 40000 --prior stick-breaking --alpha 1000 --gibbs_iters 100 --stochastic --seed 4 --nb_models 1000 --mute &
-python evaluate_sarcos.py --nb_train 40000 --prior stick-breaking --alpha 1000 --gibbs_iters 100 --stochastic --seed 5 --nb_models 1000 --mute
-
-python evaluate_sarcos.py --nb_train 40000 --prior dirichlet --alpha 1 --gibbs_iters 100 --stochastic --seed 1 --nb_models 1000 --mute &
-python evaluate_sarcos.py --nb_train 40000 --prior dirichlet --alpha 1 --gibbs_iters 100 --stochastic --seed 2 --nb_models 1000 --mute &
-python evaluate_sarcos.py --nb_train 40000 --prior dirichlet --alpha 1 --gibbs_iters 100 --stochastic --seed 3 --nb_models 1000 --mute &
-python evaluate_sarcos.py --nb_train 40000 --prior dirichlet --alpha 1 --gibbs_iters 100 --stochastic --seed 4 --nb_models 1000 --mute &
-python evaluate_sarcos.py --nb_train 40000 --prior dirichlet --alpha 1 --gibbs_iters 100 --stochastic --seed 5 --nb_models 1000 --mute
-
-python evaluate_sarcos.py --nb_train 40000 --prior dirichlet --alpha 10 --gibbs_iters 100 --stochastic --seed 1 --nb_models 1000 --mute &
-python evaluate_sarcos.py --nb_train 40000 --prior dirichlet --alpha 10 --gibbs_iters 100 --stochastic --seed 2 --nb_models 1000 --mute &
-python evaluate_sarcos.py --nb_train 40000 --prior dirichlet --alpha 10 --gibbs_iters 100 --stochastic --seed 3 --nb_models 1000 --mute &
-python evaluate_sarcos.py --nb_train 40000 --prior dirichlet --alpha 10 --gibbs_iters 100 --stochastic --seed 4 --nb_models 1000 --mute &
-python evaluate_sarcos.py --nb_train 40000 --prior dirichlet --alpha 10 --gibbs_iters 100 --stochastic --seed 5 --nb_models 1000 --mute
+#python evaluate_sarcos.py --prior dirichlet --alpha 0.1 --gibbs_iters 25 --nb_seeds 5 --nb_models 2500 --mute
+#python evaluate_sarcos.py --prior dirichlet --alpha 1 --gibbs_iters 25 --nb_seeds 5 --nb_models 2500 --mute
+#python evaluate_sarcos.py --prior dirichlet --alpha 5 --gibbs_iters 25 --nb_seeds 5 --nb_models 2500 --mute
