e176facdc24ab0dc20ee6c497586c963e25073af
Hany Abdulsamad <abdulsamad@ias.informatik.tu-darmstadt.de>
2020-04-19 15:03:56 +0200

    update robot results and add comparisons

---
+++ examples/dpglm/uai2020/robot/barrett/barrett_script.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#python evaluate_barrett_dpglm.py --prior stick-breaking --alpha 500 --gibbs_iters 25 --nb_seeds 5 --nb_models 2500 --mute
+#python evaluate_barrett_dpglm.py --prior stick-breaking --alpha 2500 --gibbs_iters 25 --nb_seeds 5 --nb_models 2500 --mute
+#python evaluate_barrett_dpglm.py --prior stick-breaking --alpha 5000 --gibbs_iters 25 --nb_seeds 5 --nb_models 2500 --mute
+
+#python evaluate_barrett_dpglm.py --prior dirichlet --alpha 0.1 --gibbs_iters 25 --nb_seeds 5 --nb_models 2500 --mute
+#python evaluate_barrett_dpglm.py --prior dirichlet --alpha 1 --gibbs_iters 25 --nb_seeds 5 --nb_models 2500 --mute
+#python evaluate_barrett_dpglm.py --prior dirichlet --alpha 5 --gibbs_iters 25 --nb_seeds 5 --nb_models 2500 --mute
