0f47f28ef03c1b556ee832fc387bc058481adeb2
Hany Abdulsamad <abdulsamad@ias.informatik.tu-darmstadt.de>
2020-06-21 20:35:55 +0200

    remove envs

--- mimo/envs/__init__.py
+++
@@ -1,7 +0,0 @@
-from .pendulum.pendulum import Pendulum
-from .pendulum.pendulum import PendulumWithCartesianObservation
-
-from .cartpole.cartpole import Cartpole
-from .cartpole.cartpole import CartpoleWithCartesianObservation
-
-from .hybrid.bouncing import BouncingBall
