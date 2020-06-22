fb16a653d6ad223d9fc5e03f0b172a7420a979e3
Hany Abdulsamad <abdulsamad@ias.informatik.tu-darmstadt.de>
2020-06-22 18:46:22 +0200

    update diag exampes

---
+++ examples/diag/ml_diag.py
@@ -0,0 +1,23 @@
+import numpy as np
+import numpy.random as npr
+
+from mimo import distributions
+
+npr.seed(1337)
+
+dim, nb_samples, nb_datasets = 3, 500, 5
+dist = distributions.GaussianWithDiagonalCovariance(mu=npr.randn(dim), sigmas=1. * npr.rand(dim))
+data = [dist.rvs(size=nb_samples) for _ in range(nb_datasets)]
+print("True mean" + "\n", dist.mu.T, "\n" + "True sigma" + "\n", dist.sigma)
+
+model = distributions.GaussianWithDiagonalCovariance(mu=np.zeros((dim, )))
+model.max_likelihood(data)
+print("ML mean"+"\n", model.mu.T, "\n"+"ML sigma"+"\n", model.sigma)
+
+hypparams = dict(mu=np.zeros((dim, )), kappas=0.05 * np.ones((dim, )),
+                 alphas=np.ones((dim, )), betas=2. * np.ones((dim, )))
+prior = distributions.NormalGamma(**hypparams)
+
+model = distributions.GaussianWithNormalGamma(prior=prior)
+model.max_aposteriori(data)
+print("ML mean"+"\n", model.likelihood.mu.T, "\n"+"ML sigma"+"\n", model.likelihood.sigma)
--- examples/diag/vi_diag.py
+++ examples/diag/vi_diag.py
@@ -6,13 +6,14 @@ from mimo import distributions
 npr.seed(1337)
 
 dim, nb_samples, nb_datasets = 3, 500, 5
-dist = distributions.GaussianWithCovariance(mu=npr.randn(dim), sigma=1. * np.diag(npr.rand(dim)))
+dist = distributions.GaussianWithDiagonalCovariance(mu=npr.randn(dim), sigmas=1. * npr.rand(dim))
 data = [dist.rvs(size=nb_samples) for _ in range(nb_datasets)]
 print("True mean" + "\n", dist.mu.T, "\n" + "True sigma" + "\n", dist.sigma)
 
-hypparams = dict(mu=np.zeros((dim, )), kappa=0.01, psi=np.eye(dim), nu=dim + 1)
-prior = distributions.NormalWishart(**hypparams)
+hypparams = dict(mu=np.zeros((dim, )), kappas=0.01 * np.ones((dim, )),
+                 alphas=np.ones((dim, )), betas=1. * np.ones((dim, )))
+prior = distributions.NormalGamma(**hypparams)
 
-model = distributions.GaussianWithNormalWishart(prior=prior)
+model = distributions.GaussianWithNormalGamma(prior=prior)
 model.meanfield_update(data)
 print("Meanfield mean"+"\n", model.likelihood.mu.T, "\n"+"Meanfield sigma"+"\n", model.likelihood.sigma)
