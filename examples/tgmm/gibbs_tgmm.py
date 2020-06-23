769676e9186483b8c0299d98f22deb667150a7aa
Hany Abdulsamad <abdulsamad@ias.informatik.tu-darmstadt.de>
2020-06-23 14:26:38 +0200

    fixed gibbs for tied

--- examples/tgmm/gibbs_tgmm.py
+++ examples/tgmm/gibbs_tgmm.py
@@ -14,14 +14,14 @@ npr.seed(1337)
 
 gating = distributions.Categorical(K=2)
 
-sigma = stats.invwishart(3, np.eye(2)).rvs()
+lmbda = stats.wishart(3, np.eye(2)).rvs()
 ensemble = distributions.TiedGaussiansWithPrecision(mus=[np.array([1., 1.]),
                                                          np.array([-1., -1.])],
-                                                    sigma=sigma)
+                                                    lmbda=lmbda)
 
 gmm = mixtures.MixtureOfTiedGaussians(gating=gating, ensemble=ensemble)
 
-obs, z = gmm.rvs(500)
+obs = [gmm.rvs(100)[0] for _ in range(5)]
 gmm.plot(obs)
 
 gating_hypparams = dict(K=2, alphas=np.ones((2, )))
@@ -29,16 +29,16 @@ gating_prior = distributions.Dirichlet(**gating_hypparams)
 
 ensemble_hypparams = dict(mus=[np.zeros((2, )) for _ in range(2)],
                           kappas=[0.01 for _ in range(2)],
-                          psi=1e0 * np.eye(2), nu=3)
-ensemble_prior = distributions.TiedNormalInverseWisharts(**ensemble_hypparams)
+                          psi=np.eye(2), nu=3)
+ensemble_prior = distributions.TiedNormalWisharts(**ensemble_hypparams)
 
 model = mixtures.BayesianMixtureOfTiedGaussians(gating=distributions.CategoricalWithDirichlet(gating_prior),
-                                                ensemble=distributions.TiedGaussiansWithNormalInverseWishart(ensemble_prior))
+                                                ensemble=distributions.TiedGaussiansWithNormalWishart(ensemble_prior))
 
 model.add_data(obs)
 
 print('Gibbs Sampling')
-for _ in progprint_xrange(2500):
+for _ in progprint_xrange(1000):
     model.resample()
 
 plt.figure()
