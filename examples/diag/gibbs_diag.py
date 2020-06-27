63658646cdc5198ca44c39450456662dc81ae096
Hany Abdulsamad <abdulsamad@ias.informatik.tu-darmstadt.de>
2020-06-27 15:53:07 +0200

    update imports in examples

--- examples/diag/gibbs_diag.py
+++ examples/diag/gibbs_diag.py
@@ -1,19 +1,21 @@
 import numpy as np
 import numpy.random as npr
 
-from mimo import distributions
+from mimo.distributions import GaussianWithDiagonalCovariance
+from mimo.distributions import NormalGamma
+from mimo.distributions import GaussianWithNormalGamma
 
 npr.seed(1337)
 
 dim, nb_samples, nb_datasets = 3, 500, 5
-dist = distributions.GaussianWithDiagonalCovariance(mu=npr.randn(dim), sigmas=1. * npr.rand(dim))
+dist = GaussianWithDiagonalCovariance(mu=npr.randn(dim), sigmas=1. * npr.rand(dim))
 data = [dist.rvs(size=nb_samples) for _ in range(nb_datasets)]
 print("True mean" + "\n", dist.mu.T, "\n" + "True sigma" + "\n", dist.sigma)
 #
 hypparams = dict(mu=np.zeros((dim, )), kappas=0.01 * np.ones((dim, )),
                  alphas=np.ones((dim, )), betas=1. * np.ones((dim, )))
-prior = distributions.NormalGamma(**hypparams)
+prior = NormalGamma(**hypparams)
 
-model = distributions.GaussianWithNormalGamma(prior=prior)
+model = GaussianWithNormalGamma(prior=prior)
 model.resample(data)
 print("Gibbs mean"+"\n", model.likelihood.mu.T, "\n"+"Gibbs sigma"+"\n", model.likelihood.sigma)
--- examples/gauss/ml_gauss.py
+++ examples/gauss/ml_gauss.py
@@ -1,23 +1,26 @@
 import numpy as np
 import numpy.random as npr
 
-from mimo import distributions
+from mimo.distributions import GaussianWithCovariance
+from mimo.distributions import GaussianWithPrecision
+from mimo.distributions import NormalWishart
+from mimo.distributions import GaussianWithNormalWishart
 
 npr.seed(1337)
 
 dim, nb_samples, nb_datasets = 3, 500, 5
-dist = distributions.GaussianWithCovariance(mu=npr.randn(dim), sigma=1. * np.diag(npr.rand(dim)))
+dist = GaussianWithCovariance(mu=npr.randn(dim), sigma=1. * np.diag(npr.rand(dim)))
 data = [dist.rvs(size=nb_samples) for _ in range(nb_datasets)]
 print("True mean" + "\n", dist.mu.T, "\n" + "True sigma" + "\n", dist.sigma)
 
-model = distributions.GaussianWithPrecision(mu=np.zeros((dim, )))
+model = GaussianWithPrecision(mu=np.zeros((dim, )))
 model.max_likelihood(data)
 print("ML mean"+"\n", model.mu.T, "\n"+"ML sigma"+"\n", model.sigma)
 
 
 hypparams = dict(mu=np.zeros((dim, )), kappa=0.05, psi=np.eye(dim), nu=dim + 1)
-prior = distributions.NormalWishart(**hypparams)
+prior = NormalWishart(**hypparams)
 
-model = distributions.GaussianWithNormalWishart(prior=prior)
+model = GaussianWithNormalWishart(prior=prior)
 model.max_aposteriori(data)
 print("MAP mean"+"\n", model.likelihood.mu.T, "\n"+"MAP sigma"+"\n", model.likelihood.sigma)
