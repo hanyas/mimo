c240ef89fa36827b2eaf85d2986bd7aa7de53e8a
Hany Abdulsamad <abdulsamad@ias.informatik.tu-darmstadt.de>
2020-06-27 15:53:07 +0200

    update imports in examples

--- examples/diag/ml_diag.py
+++ examples/diag/ml_diag.py
@@ -1,23 +1,25 @@
 import numpy as np
 import numpy.random as npr
 
-from mimo import distributions
+from mimo.distributions import GaussianWithDiagonalCovariance
+from mimo.distributions import NormalGamma
+from mimo.distributions import GaussianWithNormalGamma
 
 npr.seed(1337)
 
 dim, nb_samples, nb_datasets = 3, 500, 5
-dist = distributions.GaussianWithDiagonalCovariance(mu=npr.randn(dim), sigmas=1. * npr.rand(dim))
+dist = GaussianWithDiagonalCovariance(mu=npr.randn(dim), sigmas=1. * npr.rand(dim))
 data = [dist.rvs(size=nb_samples) for _ in range(nb_datasets)]
 print("True mean" + "\n", dist.mu.T, "\n" + "True sigma" + "\n", dist.sigma)
 
-model = distributions.GaussianWithDiagonalCovariance(mu=np.zeros((dim, )))
+model = GaussianWithDiagonalCovariance(mu=np.zeros((dim, )))
 model.max_likelihood(data)
 print("ML mean"+"\n", model.mu.T, "\n"+"ML sigma"+"\n", model.sigma)
 
 hypparams = dict(mu=np.zeros((dim, )), kappas=0.05 * np.ones((dim, )),
                  alphas=np.ones((dim, )), betas=2. * np.ones((dim, )))
-prior = distributions.NormalGamma(**hypparams)
+prior = NormalGamma(**hypparams)
 
-model = distributions.GaussianWithNormalGamma(prior=prior)
+model = GaussianWithNormalGamma(prior=prior)
 model.max_aposteriori(data)
 print("ML mean"+"\n", model.likelihood.mu.T, "\n"+"ML sigma"+"\n", model.likelihood.sigma)
--- examples/tgmm/vi_tgmm.py
+++ examples/tgmm/vi_tgmm.py
@@ -5,35 +5,42 @@ from scipy import stats
 
 from matplotlib import pyplot as plt
 
-from mimo import distributions
-from mimo import mixtures
+from mimo.distributions import Categorical
+from mimo.distributions import Dirichlet
+from mimo.distributions import CategoricalWithDirichlet
+from mimo.distributions import TiedGaussiansWithPrecision
+from mimo.distributions import TiedNormalWisharts
+from mimo.distributions import TiedGaussiansWithNormalWishart
+
+from mimo.mixtures import MixtureOfTiedGaussians
+from mimo.mixtures import BayesianMixtureOfTiedGaussians
 
 from mimo.util.text import progprint_xrange
 
 # npr.seed(1337)
 
-gating = distributions.Categorical(K=2)
+gating = Categorical(K=2)
 
 lmbda = stats.wishart(3, np.eye(2)).rvs()
-ensemble = distributions.TiedGaussiansWithPrecision(mus=[np.array([1., 1.]),
-                                                         np.array([-1., -1.])],
-                                                    lmbda=lmbda)
+ensemble = TiedGaussiansWithPrecision(mus=[np.array([1., 1.]),
+                                           np.array([-1., -1.])],
+                                      lmbda=lmbda)
 
-gmm = mixtures.MixtureOfTiedGaussians(gating=gating, ensemble=ensemble)
+gmm = MixtureOfTiedGaussians(gating=gating, ensemble=ensemble)
 
 obs = [gmm.rvs(100)[0] for _ in range(5)]
 gmm.plot(obs)
 
 gating_hypparams = dict(K=2, alphas=10 * np.ones((2, )))
-gating_prior = distributions.Dirichlet(**gating_hypparams)
+gating_prior = Dirichlet(**gating_hypparams)
 
 ensemble_hypparams = dict(mus=[np.zeros((2, )) for _ in range(2)],
                           kappas=[0.01 for _ in range(2)],
                           psi=np.eye(2), nu=3)
-ensemble_prior = distributions.TiedNormalWisharts(**ensemble_hypparams)
+ensemble_prior = TiedNormalWisharts(**ensemble_hypparams)
 
-model = mixtures.BayesianMixtureOfTiedGaussians(gating=distributions.CategoricalWithDirichlet(gating_prior),
-                                                ensemble=distributions.TiedGaussiansWithNormalWishart(ensemble_prior))
+model = BayesianMixtureOfTiedGaussians(gating=CategoricalWithDirichlet(gating_prior),
+                                       ensemble=TiedGaussiansWithNormalWishart(ensemble_prior))
 
 model.add_data(obs)
 
