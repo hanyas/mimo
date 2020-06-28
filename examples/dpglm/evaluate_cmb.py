3c34a4d489cfc6a14badee972f2f69e8232a9e95
Hany Abdulsamad <abdulsamad@ias.informatik.tu-darmstadt.de>
2020-06-28 21:44:05 +0200

    adapting dpglm examples

--- examples/dpglm/evaluate_cmb.py
+++ examples/dpglm/evaluate_cmb.py
@@ -1,14 +1,24 @@
 import os
+import argparse
+
 os.environ["OMP_NUM_THREADS"] = "1"
 
 import numpy as np
 import numpy.random as npr
 
 import mimo
-from mimo import distributions, mixture
-from mimo.util.text import progprint_xrange
+from mimo.distributions import NormalWishart
+from mimo.distributions import MatrixNormalWishart
+from mimo.distributions import GaussianWithNormalWishart
+from mimo.distributions import LinearGaussianWithMatrixNormalWishart
 
-import argparse
+from mimo.distributions import StickBreaking
+from mimo.distributions import Dirichlet
+from mimo.distributions import CategoricalWithDirichlet
+from mimo.distributions import CategoricalWithStickBreaking
+
+from mimo.mixtures import BayesianMixtureOfLinearGaussians
+from mimo.util.text import progprint_xrange
 
 import matplotlib.pyplot as plt
 
@@ -38,45 +48,44 @@ def _job(kwargs):
     models_prior = []
 
     # initialize Normal
-    psi_niw = 1e0
+    psi_nw = 1e1
     kappa = 1e-2
 
     # initialize Matrix-Normal
-    psi_mniw = 1e0
-    V = 1e3 * np.eye(nb_params)
+    psi_mnw = 1e0
+    K = 1e-1 * np.eye(nb_params)
 
     for n in range(args.nb_models):
         basis_hypparams = dict(mu=np.zeros((input_dim, )),
-                               psi=np.eye(input_dim) * psi_niw,
+                               psi=np.eye(input_dim) * psi_nw,
                                kappa=kappa, nu=input_dim + 1)
 
-        aux = distributions.NormalInverseWishart(**basis_hypparams)
+        aux = NormalWishart(**basis_hypparams)
         basis_prior.append(aux)
 
         models_hypparams = dict(M=np.zeros((target_dim, nb_params)),
-                                affine=args.affine, V=V,
-                                nu=target_dim + 1,
-                                psi=np.eye(target_dim) * psi_mniw)
+                                K=K, nu=target_dim + 1,
+                                psi=np.eye(target_dim) * psi_mnw)
 
-        aux = distributions.MatrixNormalInverseWishart(**models_hypparams)
+        aux = MatrixNormalWishart(**models_hypparams)
         models_prior.append(aux)
 
     # define gating
     if args.prior == 'stick-breaking':
         gating_hypparams = dict(K=args.nb_models, gammas=np.ones((args.nb_models,)), deltas=np.ones((args.nb_models,)) * args.alpha)
-        gating_prior = distributions.StickBreaking(**gating_hypparams)
+        gating_prior = StickBreaking(**gating_hypparams)
 
-        dpglm = mixture.BayesianMixtureOfLinearGaussians(gating=distributions.BayesianCategoricalWithStickBreaking(gating_prior),
-                                                         basis=[distributions.BayesianGaussian(basis_prior[i]) for i in range(args.nb_models)],
-                                                         models=[distributions.BayesianLinearGaussian(models_prior[i]) for i in range(args.nb_models)])
+        dpglm = BayesianMixtureOfLinearGaussians(gating=CategoricalWithStickBreaking(gating_prior),
+                                                 basis=[GaussianWithNormalWishart(basis_prior[i]) for i in range(args.nb_models)],
+                                                 models=[LinearGaussianWithMatrixNormalWishart(models_prior[i], affine=args.affine) for i in range(args.nb_models)])
 
     else:
         gating_hypparams = dict(K=args.nb_models, alphas=np.ones((args.nb_models,)) * args.alpha)
-        gating_prior = distributions.Dirichlet(**gating_hypparams)
+        gating_prior = Dirichlet(**gating_hypparams)
 
-        dpglm = mixture.BayesianMixtureOfLinearGaussians(gating=distributions.BayesianCategoricalWithDirichlet(gating_prior),
-                                                         basis=[distributions.BayesianGaussian(basis_prior[i]) for i in range(args.nb_models)],
-                                                         models=[distributions.BayesianLinearGaussian(models_prior[i]) for i in range(args.nb_models)])
+        dpglm = BayesianMixtureOfLinearGaussians(gating=CategoricalWithDirichlet(gating_prior),
+                                                 basis=[GaussianWithNormalWishart(basis_prior[i]) for i in range(args.nb_models)],
+                                                 models=[LinearGaussianWithMatrixNormalWishart(models_prior[i], affine=args.affine) for i in range(args.nb_models)])
     dpglm.add_data(target, input, whiten=True)
 
     for _ in range(args.super_iters):
@@ -144,7 +153,7 @@ if __name__ == "__main__":
     parser.add_argument('--affine', help='affine functions', action='store_true', default=True)
     parser.add_argument('--no_affine', help='non-affine functions', dest='affine', action='store_false')
     parser.add_argument('--super_iters', help='interleaving Gibbs/VI iterations', default=3, type=int)
-    parser.add_argument('--gibbs_iters', help='Gibbs iterations', default=25, type=int)
+    parser.add_argument('--gibbs_iters', help='Gibbs iterations', default=50, type=int)
     parser.add_argument('--stochastic', help='use stochastic VI', action='store_true', default=False)
     parser.add_argument('--no_stochastic', help='do not use stochastic VI', dest='stochastic', action='store_false')
     parser.add_argument('--deterministic', help='use deterministic VI', action='store_true', default=True)
@@ -209,7 +218,7 @@ if __name__ == "__main__":
 
     axes[0].scatter(sorted_input, sorted_target, s=0.75, color='k')
     axes[0].plot(sorted_input, sorted_mu_predict, color='crimson')
-    for c in [1., 2.]:
+    for c in [1., 2., 3.]:
         axes[0].fill_between(sorted_input,
                              sorted_mu_predict - c * sorted_std_predict,
                              sorted_mu_predict + c * sorted_std_predict,
