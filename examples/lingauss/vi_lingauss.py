93a8141533086c5c6da87ee839fce9ea69da5bfd
Hany Abdulsamad <abdulsamad@ias.informatik.tu-darmstadt.de>
2020-06-28 12:15:03 +0200

    finished refactoring lingauss

--- examples/lingauss/vi_lingauss.py
+++ examples/lingauss/vi_lingauss.py
@@ -1,33 +1,35 @@
 import numpy as np
 import numpy.random as npr
 
-from mimo import distributions
+from mimo.distributions import LinearGaussianWithPrecision
+from mimo.distributions import MatrixNormalWishart
+from mimo.distributions import LinearGaussianWithMatrixNormalWishart
 
-npr.seed(1337)
 
-in_dim = 50
-out_dim = 1
+# npr.seed(1337)
 
-_A = 1. * npr.randn(out_dim, in_dim)
+dcol = 50
+drow = 1
+
+A = 1. * npr.randn(drow, dcol)
 
 nb_samples = 200
-nb_datasets = 200
+nb_datasets = 10
 
-dist = distributions.LinearGaussian(A=_A, sigma=25e-2 * np.eye(out_dim), affine=False)
-x = [npr.randn(nb_samples, in_dim) for _ in range(nb_datasets)]
+dist = LinearGaussianWithPrecision(A=A, lmbda=100 * np.eye(drow), affine=False)
+x = [npr.randn(nb_samples, dcol) for _ in range(nb_datasets)]
 y = [dist.rvs(_x) for _x in x]
 print("True transf."+"\n", dist.A, "\n"+"True sigma"+"\n", dist.sigma)
 
 affine = False
-n_params = in_dim + 1 if affine else in_dim
+nb_params = dcol + 1 if affine else dcol
 
-hypparams = dict(M=np.zeros((out_dim, n_params)),
-                 V=1. * np.eye(n_params),
-                 affine=affine,
-                 psi=np.eye(out_dim),
-                 nu=2 * out_dim + 1)
-prior = distributions.MatrixNormalInverseWishart(**hypparams)
+hypparams = dict(M=np.zeros((drow, nb_params)),
+                 K=1e-2 * np.eye(nb_params),
+                 psi=np.eye(drow),
+                 nu=drow + 1)
+prior = MatrixNormalWishart(**hypparams)
 
-model = distributions.LinearGaussianWithMatrixNormalInverseWishart(prior)
+model = LinearGaussianWithMatrixNormalWishart(prior, affine=False)
 model.meanfield_update(y=y, x=x)
-print("Meanfield transf."+"\n", model.A, "\n"+"Meanfield sigma"+"\n", model.sigma)
+print("ML transf."+"\n", model.likelihood.A, "\n"+"ML covariance"+"\n", model.likelihood.sigma)
