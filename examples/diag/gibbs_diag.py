4a2b31ccaf9458de996fa7f2a1449d9b7831732f
Hany Abdulsamad <abdulsamad@ias.informatik.tu-darmstadt.de>
2020-07-15 16:29:34 +0200

    update diagonal examples

--- examples/diag/gibbs_diag.py
+++ examples/diag/gibbs_diag.py
@@ -5,15 +5,15 @@ from mimo.distributions import GaussianWithDiagonalCovariance
 from mimo.distributions import NormalGamma
 from mimo.distributions import GaussianWithNormalGamma
 
-npr.seed(1337)
+# npr.seed(1337)
 
 dim, nb_samples, nb_datasets = 3, 500, 5
 dist = GaussianWithDiagonalCovariance(mu=npr.randn(dim), sigmas=1. * npr.rand(dim))
 data = [dist.rvs(size=nb_samples) for _ in range(nb_datasets)]
 print("True mean" + "\n", dist.mu.T, "\n" + "True sigma" + "\n", dist.sigma)
 #
-hypparams = dict(mu=np.zeros((dim, )), kappas=0.01 * np.ones((dim, )),
-                 alphas=np.ones((dim, )), betas=1. * np.ones((dim, )))
+hypparams = dict(mu=np.zeros((dim, )), kappas=1e-2 * np.ones((dim, )),
+                 alphas=1. * np.ones((dim, )), betas=1. / 2. * np.ones((dim, )))
 prior = NormalGamma(**hypparams)
 
 model = GaussianWithNormalGamma(prior=prior)
