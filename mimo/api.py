cebd4c4b3ccf83fb6d26189e31bce0dd8049c9b3
Hany Abdulsamad <abdulsamad@ias.informatik.tu-darmstadt.de>
2020-07-16 13:40:29 +0200

    update api

--- mimo/api.py
+++ mimo/api.py
@@ -31,6 +31,14 @@ class ILR(object):
     model_psi: float
     model_K: float
 
+    super_iters: int
+    gibbs_iters: int
+    vi_iters: int
+    vi_earlystop: float
+    svi_iters: int
+    svi_batch_size: int
+    svi_step_size: float
+
 
 class InfiniteLinearRegression:
     """
@@ -40,7 +48,11 @@ class InfiniteLinearRegression:
                  nb_models, affine,
                  gating_alpha,
                  basis_psi, basis_kappa,
-                 model_psi, model_K):
+                 model_psi, model_K,
+                 super_iters, gibbs_iters,
+                 vi_iters, vi_earlystop,
+                 svi_iters, svi_batch_size,
+                 svi_step_size):
         """
         The constructor for RegressionModel.
 
@@ -59,50 +71,58 @@ class InfiniteLinearRegression:
         # number of parameters per local regressor
         self.nb_params = self.k + 1 if self.affine else self.k
 
-        basis_prior = []
-        model_prior = []
+        self.basis_prior = []
+        self.model_prior = []
 
         for n in range(self.nb_models):
             basis_hypparams = dict(mu=np.zeros((self.k,)),
                                    psi=np.eye(self.k) * basis_psi,
                                    kappa=basis_kappa, nu=self.k + 2)
 
-            basis_prior.append(NormalWishart(**basis_hypparams))
+            self.basis_prior.append(NormalWishart(**basis_hypparams))
 
             model_hypparams = dict(M=np.zeros((self.d, self.nb_params)),
                                    K=np.eye(self.nb_params) * model_K,
                                    psi=np.eye(self.d) * model_psi, nu=self.d + 2)
 
-            model_prior.append(MatrixNormalWishart(**model_hypparams))
+            self.model_prior.append(MatrixNormalWishart(**model_hypparams))
 
         gating_hypparams = dict(K=self.nb_models,
                                 gammas=np.ones((self.nb_models,)),
                                 deltas=np.ones((self.nb_models,)) * gating_alpha)
-        gating_prior = StickBreaking(**gating_hypparams)
+        self.gating_prior = StickBreaking(**gating_hypparams)
 
-        self.regressor = BayesianMixtureOfLinearGaussians(gating=CategoricalWithStickBreaking(gating_prior),
-                                                          basis=[GaussianWithNormalWishart(basis_prior[i]) for i in range(self.nb_models)],
-                                                          models=[LinearGaussianWithMatrixNormalWishart(model_prior[i], affine=self.affine)
-                                                                  for i in range(self.nb_models)])
+        self.regressor = None
 
-    def fit(self, output, input,
-            super_iters=1, verbose=False,
-            gibbs_iters=1, vi_iters=1000,
-            vi_earlystop=1e-2, svi_iters=0,
-            svi_batch_size=64, svi_step_size=1e-3):
+        self.super_iters = super_iters
+        self.gibbs_iters = gibbs_iters
+
+        self.vi_iters = vi_iters
+        self.vi_earlystop = vi_earlystop
+
+        self.svi_iters = svi_iters
+        self.svi_batch_size = svi_batch_size
+        self.svi_step_size = svi_step_size
+
+    def fit(self, input, output, verbose=True):
         """
         Fits the model to the training data.
         """
 
+        self.regressor = BayesianMixtureOfLinearGaussians(gating=CategoricalWithStickBreaking(self.gating_prior),
+                                                          basis=[GaussianWithNormalWishart(self.basis_prior[i]) for i in range(self.nb_models)],
+                                                          models=[LinearGaussianWithMatrixNormalWishart(self.model_prior[i], affine=self.affine)
+                                                                  for i in range(self.nb_models)])
+
         self.regressor.add_data(output, input, whiten=False)
 
-        for i in range(super_iters):
+        for i in range(self.super_iters):
             # Gibbs sampling
             if verbose:
                 print("Gibbs Sampling")
 
-            gibbs_iter = range(gibbs_iters) if not verbose \
-                else progprint_xrange(gibbs_iters)
+            gibbs_iter = range(self.gibbs_iters) if not verbose \
+                else progprint_xrange(self.gibbs_iters)
 
             for _ in gibbs_iter:
                 self.regressor.resample()
@@ -111,29 +131,28 @@ class InfiniteLinearRegression:
             if verbose:
                 print('Stochastic Variational Inference')
 
-            svi_iter = range(svi_iters) if not verbose \
-                else progprint_xrange(svi_iters)
+            svi_iter = range(self.svi_iters) if not verbose \
+                else progprint_xrange(self.svi_iters)
 
-            prob = svi_batch_size / float(len(input))
+            prob = self.svi_batch_size / float(len(input))
             for _ in svi_iter:
-                minibatch = npr.permutation(len(input))[:svi_batch_size]
+                minibatch = npr.permutation(len(input))[:self.svi_batch_size]
                 self.regressor.meanfield_sgdstep(y=output[minibatch, :], x=input[minibatch, :],
-                                                 prob=prob, stepsize=svi_step_size)
+                                                 prob=prob, stepsize=self.svi_step_size)
 
             # Meanfield VI
             if verbose:
                 print("Variational Inference")
-            self.regressor.meanfield_coordinate_descent(tol=vi_earlystop,
-                                                        maxiter=vi_iters,
+            self.regressor.meanfield_coordinate_descent(tol=self.vi_earlystop,
+                                                        maxiter=self.vi_iters,
                                                         progprint=verbose)
 
-            if super_iters > 1 and i + 1 < super_iters:
+            if self.super_iters > 1 and i + 1 < self.super_iters:
                 self.regressor.gating.prior = self.regressor.gating.posterior
                 for n in range(self.regressor.size):
                     self.regressor.basis[n].prior = self.regressor.basis[n].posterior
                     self.regressor.models[n].prior = self.regressor.models[n].posterior
 
     def predict(self, X):
-        mu, var, _, _ = self.regressor.meanfield_prediction(X, prediction='average')
+        mu, var, _ = self.regressor.meanfield_prediction(X, prediction='average')
         return mu, var
-
