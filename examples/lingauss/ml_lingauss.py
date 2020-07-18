93d7ed3efcc4b28dd01144c7247e9970e7229e9c
Hany Abdulsamad <abdulsamad@ias.informatik.tu-darmstadt.de>
2020-07-18 18:00:38 +0200

    add elatoric noise to api

--- examples/lingauss/ml_lingauss.py
+++ examples/lingauss/ml_lingauss.py
@@ -3,24 +3,32 @@ import numpy.random as npr
 
 from mimo.distributions import LinearGaussianWithPrecision
 
+import time
 
 npr.seed(1337)
 
+
 dcol = 50
-drow = 1
+drow = 22
 
 A = 1. * npr.randn(drow, dcol)
 
-nb_samples = 200
-nb_datasets = 10
+nb_samples = 50000
+nb_datasets = 1
 
 dist = LinearGaussianWithPrecision(A=A, lmbda=100 * np.eye(drow), affine=False)
 x = [npr.randn(nb_samples, dcol) for _ in range(nb_datasets)]
 y = [dist.rvs(_x) for _x in x]
-print("True transf."+"\n", dist.A, "\n"+"True sigma"+"\n", dist.sigma)
+# print("True transf."+"\n", dist.A, "\n"+"True sigma"+"\n", dist.sigma)
 
 affine = False
 
 model = LinearGaussianWithPrecision(affine=False)
-model.max_likelihood(y=y, x=x)
-print("ML transf."+"\n", model.A, "\n"+"ML covariance"+"\n", model.sigma)
+
+start = time.time()
+for _ in range(100):
+    model.max_likelihood(y=y, x=x)
+# print("ML transf."+"\n", model.A, "\n"+"ML covariance"+"\n", model.sigma)
+
+stop = time.time()
+print(stop - start)
