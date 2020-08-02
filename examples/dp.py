1ebeded293dbe0ce0b643b06ad666314126951eb
Hany Abdulsamad <abdulsamad@ias.informatik.tu-darmstadt.de>
2020-08-02 14:42:56 +0200

    add dp example

---
+++ examples/dp.py
@@ -0,0 +1,35 @@
+import numpy as np
+import numpy.random as npr
+
+import scipy as sc
+from scipy import stats
+
+from mimo.distributions import StickBreaking
+
+alpha = 5
+K = 5
+
+sb = StickBreaking(K, np.ones(K), np.ones(K) * alpha)
+
+weights = np.vstack([sb.rvs() for _ in range(10000)])
+weights = np.mean(weights, axis=0)
+
+
+import matplotlib.pyplot as plt
+
+plt.axis([0, K + 1, 0, np.max(weights)])
+plt.bar(range(1, K + 1), weights)
+plt.show()
+
+
+h = sc.stats.norm
+omega = h.rvs(size=(10, K))
+
+x = np.linspace(-3, 3, 200)
+sample_cdfs = (weights[..., np.newaxis] * np.less.outer(omega, x)).sum(axis=1)
+
+fig, ax = plt.subplots(figsize=(8, 6))
+
+ax.plot(x, sample_cdfs[1:].T, c='gray', alpha=0.75)
+ax.plot(x, h.cdf(x), c='k', label='Base CDF')
+plt.show()
