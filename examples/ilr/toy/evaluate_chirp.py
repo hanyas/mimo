da4944154407325f04b754a455ba0b7e8cab357d
Hany Abdulsamad <abdulsamad@ias.informatik.tu-darmstadt.de>
2020-10-28 11:58:48 +0100

    update examples

--- examples/ilr/toy/evaluate_chirp.py
+++ examples/ilr/toy/evaluate_chirp.py
@@ -16,8 +16,6 @@ from mimo.distributions import GaussianWithNormalWishart
 from mimo.distributions import LinearGaussianWithMatrixNormalWishart
 
 from mimo.distributions import TruncatedStickBreaking
-from mimo.distributions import Dirichlet
-from mimo.distributions import CategoricalWithDirichlet
 from mimo.distributions import CategoricalWithStickBreaking
 
 from mimo.mixtures import BayesianMixtureOfLinearGaussians
@@ -35,15 +33,9 @@ if __name__ == "__main__":
     parser.add_argument('--nb_models', help='max number of models', default=50, type=int)
     parser.add_argument('--affine', help='affine functions', action='store_true', default=True)
     parser.add_argument('--no_affine', help='non-affine functions', dest='affine', action='store_false')
-    parser.add_argument('--super_iters', help='interleaving Gibbs/VI iterations', default=3, type=int)
-    parser.add_argument('--gibbs_iters', help='Gibbs iterations', default=50, type=int)
-    parser.add_argument('--stochastic', help='use stochastic VI', action='store_true', default=False)
-    parser.add_argument('--no_stochastic', help='do not use stochastic VI', dest='stochastic', action='store_false')
-    parser.add_argument('--deterministic', help='use deterministic VI', action='store_true', default=True)
-    parser.add_argument('--no_deterministic', help='do not use deterministic VI', dest='deterministic', action='store_false')
-    parser.add_argument('--meanfield_iters', help='max VI iterations', default=25, type=int)
-    parser.add_argument('--svi_iters', help='SVI iterations', default=500, type=int)
-    parser.add_argument('--svi_stepsize', help='SVI step size', default=0.9, type=float)
+    parser.add_argument('--super_iters', help='interleaving VI iterations', default=3, type=int)
+    parser.add_argument('--gibbs_iters', help='Gibbs iterations', default=1, type=int)
+    parser.add_argument('--meanfield_iters', help='max VI iterations', default=50, type=int)
     parser.add_argument('--prediction', help='prediction w/ mode or average', default='average')
     parser.add_argument('--earlystop', help='stopping criterion for VI', default=1e-2, type=float)
     parser.add_argument('--verbose', help='show learning progress', action='store_true', default=True)
@@ -67,6 +59,14 @@ if __name__ == "__main__":
 
     input, target = data[:, :1], data[:, 1:]
 
+    # scale data
+    from sklearn.preprocessing import StandardScaler
+    input_transform = StandardScaler()
+    target_transform = StandardScaler()
+
+    input_transform.fit(input)
+    target_transform.fit(target)
+
     # prepare model
     input_dim, target_dim = 1, 1
 
@@ -74,16 +74,9 @@ if __name__ == "__main__":
     if args.affine:
         nb_params += 1
 
-    basis_prior = []
-    models_prior = []
-
-    # initialize Normal
-    psi_nw = 1e0
-    kappa = 1e-2
-
-    # initialize Matrix-Normal
-    psi_mnw = 1e0
-    K = 1e-2
+    basis_prior, models_prior = [], []
+    psi_nw, kappa = 1e0, 1e-2
+    psi_mnw, K = 1e0, 1e-2
 
     for n in range(args.nb_models):
         basis_hypparams = dict(mu=np.zeros((input_dim,)),
@@ -100,81 +93,56 @@ if __name__ == "__main__":
         aux = MatrixNormalWishart(**models_hypparams)
         models_prior.append(aux)
 
-    # define gating
-    if args.prior == 'stick-breaking':
-        gating_hypparams = dict(K=args.nb_models, gammas=np.ones((args.nb_models,)),
-                                deltas=np.ones((args.nb_models,)) * args.alpha)
-        gating_prior = TruncatedStickBreaking(**gating_hypparams)
+    gating_hypparams = dict(K=args.nb_models, gammas=np.ones((args.nb_models,)),
+                            deltas=np.ones((args.nb_models,)) * args.alpha)
+    gating_prior = TruncatedStickBreaking(**gating_hypparams)
 
-        ilr = BayesianMixtureOfLinearGaussians(gating=CategoricalWithStickBreaking(gating_prior),
-                                               basis=[GaussianWithNormalWishart(basis_prior[i])
-                                                      for i in range(args.nb_models)],
-                                               models=[LinearGaussianWithMatrixNormalWishart(models_prior[i], affine=args.affine)
-                                                       for i in range(args.nb_models)])
+    ilr = BayesianMixtureOfLinearGaussians(gating=CategoricalWithStickBreaking(gating_prior),
+                                           basis=[GaussianWithNormalWishart(basis_prior[i])
+                                                  for i in range(args.nb_models)],
+                                           models=[LinearGaussianWithMatrixNormalWishart(models_prior[i], affine=args.affine)
+                                                   for i in range(args.nb_models)])
 
-    else:
-        gating_hypparams = dict(K=args.nb_models, alphas=np.ones((args.nb_models,)) * args.alpha)
-        gating_prior = Dirichlet(**gating_hypparams)
-
-        ilr = BayesianMixtureOfLinearGaussians(gating=CategoricalWithDirichlet(gating_prior),
-                                               basis=[GaussianWithNormalWishart(basis_prior[i])
-                                                      for i in range(args.nb_models)],
-                                               models=[LinearGaussianWithMatrixNormalWishart(models_prior[i], affine=args.affine)
-                                                       for i in range(args.nb_models)])
+    import copy
+    from sklearn.utils import shuffle
+    from sklearn.metrics import mean_squared_error, r2_score
 
     anim = []
 
-    from sklearn.preprocessing import StandardScaler
-
-    input_transform = StandardScaler()
-    target_transform = StandardScaler()
-
-    input_transform.fit(input)
-    target_transform.fit(target)
-
     split_size = int(nb_train / args.nb_splits)
+
+    mse = np.zeros((args.nb_splits, ))
+    smse = np.zeros((args.nb_splits, ))
+    nb_models = np.zeros((args.nb_splits, ), dtype=np.int64)
+
     for n in range(args.nb_splits):
         print('Processing data split ' + str(n + 1) + ' out of ' + str(args.nb_splits))
 
-        # remove old data
-        try:
+        # clear all previous data
+        if n > 0:
             ilr.clear_data()
-        except IndexError:
-            print('Model has no data')
 
-        _input = input[n * split_size: (n + 1) * split_size, :]
-        _target = target[n * split_size: (n + 1) * split_size, :]
+        train_input = input[n * split_size: (n + 1) * split_size, :]
+        train_target = target[n * split_size: (n + 1) * split_size, :]
 
-        ilr.add_data(y=_target, x=_input,
-                     whiten=True,
+        ilr.add_data(y=train_target, x=train_input,
+                     whiten=True, transform_type='Standard',
                      input_transform=input_transform,
                      target_transform=target_transform)
-
-        # set posterior to prior
-        import copy
 
         ilr.gating.prior = copy.deepcopy(ilr.gating.posterior)
         for i in range(ilr.likelihood.size):
             ilr.basis[i].prior = copy.deepcopy(ilr.basis[i].posterior)
             ilr.models[i].prior = copy.deepcopy(ilr.models[i].posterior)
 
-        # Gibbs sampling
         ilr.resample(maxiter=args.gibbs_iters)
 
-        # train model
         for _ in range(args.super_iters):
-            if args.stochastic:
-                # Stochastic meanfield VI
-                ilr.meanfield_stochastic_descent(stepsize=args.svi_stepsize,
-                                                 batchsize=split_size,
-                                                 maxiter=1)
-            if args.deterministic:
-                # Meanfield VI
-                ilr.meanfield_coordinate_descent(tol=args.earlystop,
-                                                 maxiter=args.meanfield_iters,
-                                                 progprint=args.verbose)
+            vlb = ilr.meanfield_coordinate_descent(tol=args.earlystop,
+                                                   maxiter=args.meanfield_iters,
+                                                   progprint=args.verbose)
+            # print(vlb[-1])
 
-            # empirical Bayes
             ilr.gating.prior = ilr.gating.posterior
             for i in range(ilr.likelihood.size):
                 ilr.basis[i].prior = ilr.basis[i].posterior
@@ -183,20 +151,19 @@ if __name__ == "__main__":
         # predict on all data
         mu, var, std = ilr.meanfield_prediction(x=input, prediction=args.prediction)
 
-        mu = np.hstack(mu)
-        var = np.hstack(var)
-        std = np.hstack(std)
+        mse[n] = mean_squared_error(target, mu)
+        smse[n] = 1. - r2_score(target, mu)
 
         # plot prediction
         fig = plt.figure(figsize=(12, 4))
         plt.scatter(input, target, s=0.75, color='k')
-        plt.axvspan(_input.min(), _input.max(), facecolor='grey', alpha=0.1)
+        plt.axvspan(train_input.min(), train_input.max(), facecolor='grey', alpha=0.1)
         plt.plot(input, mu, color='crimson')
 
         for c in [1., 2.]:
             plt.fill_between(input.flatten(),
-                             mu - c * std,
-                             mu + c * std,
+                             mu.flatten() - c * std.flatten(),
+                             mu.flatten() + c * std.flatten(),
                              color=(0, 0, 1, 0.05))
 
         plt.ylim((-2.5, 2.5))
